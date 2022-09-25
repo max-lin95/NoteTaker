31a75516e14c0c783f41a877e200d6bc59225735
Jeremy Lin <maxlinjr95@gmail.com>
2022-09-25 13:19:34 -0700

    createNewNote

--- Develop/server.js
+++ Develop/server.js
@@ -9,3 +9,33 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './develop/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './develop/public/index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(noteReq.slice(1));
+});
+
+function enterNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './develop/db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+
+    return newNote;
+};
