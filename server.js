07c06a029021f543ce5ba5c305916adb7d9a1a66
Jeremy Lin <maxlinjr95@gmail.com>
2022-09-25 13:45:54 -0700

    paths changed

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/api/notes', (req, res) => {
@@ -33,7 +33,7 @@ function enterNewNote(body, notesArray) {
 
     notesArray.push(newNote);
     fs.writeFileSync(
-        path.join(__dirname, './develop/db/db.json'),
+        path.join(__dirname, './db/db.json'),
         JSON.stringify(notesArray, null, 2)
     );
 
@@ -52,7 +52,7 @@ function deleteNote(id, notesArray) {
         if(note.id === id) {
             notesArray.splice(i, 1);
             fs.writeFileSync(
-                path.join(__dirname, './develop/db/db.json'),
+                path.join(__dirname, './db/db.json'),
                 JSON.stringify(notesArray, null, 2)
             );
             break;
